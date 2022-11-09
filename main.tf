07933f146f8550c9c5455034bf565b1b86318db2
Matt Schiller <mschiller@banyansecurity.io>
2022-11-09 12:44:33 -0800

    update to 22.04

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
   filter {
@@ -87,7 +87,7 @@ resource "aws_security_group" "sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = distinct(concat([data.aws_vpc.selected.cidr_block], var.management_cidrs))
+    cidr_blocks = var.management_cidrs
     description = "SSH"
   }
 
