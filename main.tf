0f8521b1e6dd1b63471df0a7c9872bb73248a71d
Patrick Jain-Taylor <pjain-taylor@lyrahealth.com>
2023-03-09 09:05:32 -0800

    change: readd trailing hyphen to LC name

--- main.tf
+++ main.tf
@@ -141,7 +141,7 @@ resource "aws_autoscaling_group" "asg" {
 }
 
 resource "aws_launch_configuration" "conf" {
-  name_prefix     = "${var.name}-accesstier-conf"
+  name_prefix     = "${var.name}-accesstier-conf-"
   image_id        = data.aws_ami.ubuntu.id
   instance_type   = var.instance_type
   key_name        = var.ssh_key_name
